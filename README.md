26c9e76c4f6da2488134f783e26e41b53a116e4b
Paul Knapp <ppankluap@gmail.com>
2025-03-13 10:02:16 -0500

    Fix: forgot to commit

--- README.md
+++ README.md
@@ -10,3 +10,37 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+# Tea Shop
+This application allows for viewing subscriptions to a tea service from an administrator perspective, this includes the ability to see subscription details including customer information
+
+# Tech Stack:
+- React
+- React Router
+- Cypress
+- Ruby on Rails
+- Shoulda Matchers
+- Rspec
+
+# Setup & Running Instructions:
+- Clone down the Tea Shop backend with the following command:
+`git clone git@github.com:Paul-Knapp/Tea-Shop-Final.git`
+- Change into the backend directly with the following command:
+`cd Tea-Shop-Final`
+- Install all the required gems for the backend with the command:
+`bundle install`
+- Start the backend with the following command:
+`rails server`
+- Clone this repo down to your local hard drive with the following command: 
+ `git clone git@github.com:Paul-Knapp/Tea-shop-FE.git`
+- Change into the project directory with the following command:
+`cd Tea-Shop-FE`
+- Start the Frontend Server with the following command:
+`npm install` & `npm start`
+
+# Screenshots:
+
+
+## Paul Knapp
+[Github](https://github.com/Paul-Knapp)
+[LinkedIn](https://www.linkedin.com/in/paul-m-knapp/)
--- src/SubscriptionDetails/SubscriptionDetails.jsx
+++ src/SubscriptionDetails/SubscriptionDetails.jsx
@@ -8,25 +8,34 @@ function SubscriptionDetails() {
     const { subscriptionId } = useParams();
     const [message, setMessage] = useState("Loading...");
     const [customer, setCustomer] = useState(null);
+    const [teas, setTeas] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:3000/api/v1/subscriptions/${subscriptionId}`)
             .then((response) => {
                 if (!response.ok) {
-                    throw new Error("Network response was not ok")
+                    throw new Error("Network response was not ok");
                 }
                 return response.json();
             })
             .then((data) => {
-                setSubscription(data.data)
-            const customerId = data.data.relationships.customer.data.id;
-            const foundCustomer = data.included.find((item) => item.id === customerId && item.type === "customer").attributes;
-            setCustomer(foundCustomer);
+                setSubscription(data.data);
+
+                const customerId = data.data.relationships.customer.data.id;
+                const foundCustomer = data.included.find(
+                    (item) => item.id === customerId && item.type === "customer"
+                )?.attributes;
+                setCustomer(foundCustomer);
+
+                const teaList = data.included
+                    .filter((item) => item.type === "tea")
+                    .map((tea) => tea.attributes);
+                setTeas(teaList);
             })
             .catch(() => {
                 setMessage("There was an error fetching data. Please try again.");
             });
-    },[subscriptionId]);
+    }, [subscriptionId]);
 
     function cancelSubscription() {
         fetch(`http://localhost:3000/api/v1/subscriptions/${subscriptionId}`, {
@@ -37,33 +46,58 @@ function SubscriptionDetails() {
             body: JSON.stringify({
                 status: "cancelled",
             }),
-        })
+        });
     }
 
-  return (
-    <div className='subscription-details-page'>
-        {subscription ?   
-        <section>
-            <section className = 'subscription-details'>
-                <NavBar/>
-                <img src={subscription.attributes.image_url} alt={subscription.attributes.title}/>
-                <h1>{subscription.attributes.title}</h1> 
-                <p>{subscription.attributes.price}</p>
-                <p>{subscription.attributes.frequency}</p>
-                <p>{subscription.attributes.description}</p>
-                <p>{subscription.attributes.status}</p>
-                <button onClick={cancelSubscription}>Cancel Subscription</button>
-            </section>
-            <section>
-                <h2>Customer Information</h2>
-                <p>{customer.first_name} {customer.last_name}</p>
-                <p>{customer.email}</p>
-                <p>{customer.address}</p>
-            </section>
-        </section>
-        : <p>{message}</p>}
-    </div>
-  );
+    return (
+        <div className='subscription-details-page'>
+            {subscription ? (
+                <section>
+                    <section className='subscription-details'>
+                        <NavBar />
+                        <img src={subscription.attributes.image_url} alt={subscription.attributes.title} />
+                        <h1>{subscription.attributes.title}</h1>
+                        <p>{subscription.attributes.price}</p>
+                        <p>{subscription.attributes.frequency}</p>
+                        <p>{subscription.attributes.description}</p>
+                        <p>{subscription.attributes.status}</p>
+                        <button onClick={cancelSubscription}>Cancel Subscription</button>
+                    </section>
+                    <section className='customer-information'>
+                        <h2>Customer Information</h2>
+                        {customer ? (
+                            <>
+                                <p>{customer.first_name} {customer.last_name}</p>
+                                <p>{customer.email}</p>
+                                <p>{customer.address}</p>
+                            </>
+                        ) : (
+                            <p>Loading customer information...</p>
+                        )}
+                    </section>
+                    <section className='tea-list'>
+                        <h2>Teas</h2>
+                        {teas.length > 0 ? (
+                            <ul>
+                                {teas.map((tea) => (
+                                    <li>
+                                        <h3>Tea Name {tea.title}</h3>
+                                        <p>{tea.description}</p>
+                                        <p>Brew Temperature: {tea.temperature}°F</p>
+                                        <p>Brew Time: {tea.brew_time} minutes</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>No teas found for this subscription.</p>
+                        )}
+                    </section>
+                </section>
+            ) : (
+                <p>{message}</p>
+            )}
+        </div>
+    );
 }
 
 export default SubscriptionDetails;
